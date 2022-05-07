a57cb8f0b31a2fb02caf121cc589e35a5ede4822
mailsonbc <mailsonbc@gmail.com>
2022-05-06 22:37:26 -0300

    Situação comfrequência e craição do projeto Jokenpo

--- calc-imc/Academia/Imc/Program.cs
+++ calc-imc/Academia/Imc/Program.cs
@@ -2,11 +2,13 @@
 {
     static void Main(string[] args)
     {
+        //TODO: Mostar a classificação do imc
+
         double peso;
         double altura;
         double imc;
         string nome;
-        string situacao = "Você está magro(a)!";
+        
 
         Console.WriteLine("Digite o seu nome: ");
         nome = Console.ReadLine();
@@ -17,18 +19,14 @@
         Console.WriteLine("Digite a sua altura: ");
         altura = Convert.ToDouble(Console.ReadLine());
 
-        //imc = peso / (altura * altura);
 
         imc = peso / Math.Pow(altura, 2);
 
-        if (imc > 30)
-        {
-            situacao = "Você está gordo(a)!";
-        }
+        
 
         Console.WriteLine("==========================");
         Console.WriteLine("{0}, seu IMC é : {1}", nome, imc.ToString("#0.00"));
-        Console.WriteLine(situacao);
+        
 
         Console.ReadLine();
     }
---
+++ calc-imc/Academia/SituacaoAlunoComFrequenciaENota/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Double nota1, nota2, media, frequencia, quantidade_aulas, quantidade_faltas;
+
+        Console.WriteLine("Situação Final do Aluno");
+        Console.WriteLine("=======================");
+
+        Console.WriteLine("Entre com a primeira nota: ");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Entre com a segunda nota: ");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de aulas: ");
+        quantidade_aulas = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de faltas: ");
+        quantidade_faltas = Convert.ToDouble(Console.ReadLine());
+
+        media = (nota1 + nota2) / 2;
+
+        //frequencia = 100 - ((quantidade_faltas / 100) * quantidade_aulas);
+        frequencia = 100 - ((quantidade_faltas / quantidade_aulas) * 100);
+
+        if((media >= 70) && (frequencia >= 75))
+        {
+            Console.WriteLine("APROVADO");
+        }
+        else
+        {
+            Console.WriteLine("REPROVADO");
+        }
+
+        Console.WriteLine(media);
+        Console.WriteLine(frequencia);
+
+
+    }
+}
