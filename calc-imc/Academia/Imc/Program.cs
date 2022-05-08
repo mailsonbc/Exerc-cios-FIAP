95d79df02dd38ae0e81c61a11e2bdf4dc37dd761
mailsonbc <mailsonbc@gmail.com>
2022-05-08 14:11:24 -0300

    Adicionado if's no exercício de imc

--- calc-imc/Academia/Imc/Program.cs
+++ calc-imc/Academia/Imc/Program.cs
@@ -2,16 +2,9 @@
 {
     static void Main(string[] args)
     {
-        //TODO: Mostar a classificação do imc
-
         double peso;
         double altura;
         double imc;
-        string nome;
-        
-
-        Console.WriteLine("Digite o seu nome: ");
-        nome = Console.ReadLine();
         
         Console.WriteLine("Digite o seu peso: ");
         peso = Convert.ToDouble(Console.ReadLine());
@@ -21,11 +14,38 @@
 
 
         imc = peso / Math.Pow(altura, 2);
+        Console.WriteLine("==========================");
+
+
+        if (imc <= 18.5)
+        {            
+            Console.WriteLine("Seu IMC é : {0}", imc.ToString("#0.00"));
+            Console.WriteLine("Você está abaixao do peso!");
+        }else if((imc >= 18.6) && (imc <= 24.9))
+        {        
+            Console.WriteLine("Seu IMC é : {0}", imc.ToString("#0.00"));
+            Console.WriteLine("Você está com o peso ideal (parabéns)!");
+        }else if((imc >= 25) && (imc <= 29.9))
+        {            
+            Console.WriteLine("Seu IMC é : {0}", imc.ToString("#0.00"));
+            Console.WriteLine("Você está levemente acima do peso!");
+        }else if((imc >= 30) && (imc <= 34.9))
+        {            
+            Console.WriteLine("Seu IMC é : {0}", imc.ToString("#0.00"));
+            Console.WriteLine("Você está com obesidade grau I!");
+        }else if((imc >= 35) && (imc <= 39.9))
+        {            
+            Console.WriteLine("Seu IMC é : {0}", imc.ToString("#0.00"));
+            Console.WriteLine("Você está com obesidade grau II (severa)!");
+        }else if(imc >= 40)
+        {           
+            Console.WriteLine("Seu IMC é : {0}", imc.ToString("#0.00"));
+            Console.WriteLine("Você está com obesidade grau III (móbida)!");
+        }
 
         
 
         Console.WriteLine("==========================");
-        Console.WriteLine("{0}, seu IMC é : {1}", nome, imc.ToString("#0.00"));
         
 
         Console.ReadLine();
