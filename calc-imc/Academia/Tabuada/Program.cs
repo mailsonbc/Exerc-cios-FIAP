6e80e16e03dec4c873dba985845e9eb0ca474f7c
mailsonbc <mailsonbc@gmail.com>
2022-05-12 20:29:05 -0300

    tabuada com for

--- calc-imc/Academia/Tabuada/Program.cs
+++ calc-imc/Academia/Tabuada/Program.cs
@@ -9,11 +9,16 @@ class Program
         Console.WriteLine("Qual a tabuada? ");
         fator01 = Convert.ToInt32(Console.ReadLine());
 
-        while(fator02 <= 10)
+        while (fator02 <= 10)
         {
             Console.WriteLine("{0} x {1} = {2}", fator01, fator02, fator01 * fator02);
 
             fator02++;
         }
+
+        //for(fator02 = 0; fator02 <= 10; fator02++)
+        //{
+        //    Console.WriteLine("{0} x {1} = {2}", fator01, fator02, fator01 * fator02);
+        //}
     }
 }
