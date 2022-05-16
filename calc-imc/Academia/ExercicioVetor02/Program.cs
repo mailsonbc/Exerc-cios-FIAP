e6ae15237de85663b908ccf18c4f09b5a1882224
mailsonbc <mailsonbc@gmail.com>
2022-05-16 20:24:57 -0300

    Refatorado

--- calc-imc/Academia/ExercicioVetor02/Program.cs
+++ calc-imc/Academia/ExercicioVetor02/Program.cs
@@ -9,7 +9,7 @@ class Program
         int[] matrizC = new int[5];
 
         Console.WriteLine("Digite 5 valores inteiros para a Matriz A:");
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < matrizA.Length; i++)
         {
             Console.WriteLine("Digite valor {0} ", i + 1);
             matrizA[i] = Convert.ToInt32(Console.ReadLine());
@@ -18,20 +18,20 @@ class Program
         Console.WriteLine();
 
         Console.WriteLine("Digite 5 valores inteiros para a Matriz B:");
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < matrizB.Length; j++)
         {
             Console.WriteLine("Digite valor {0} ", j + 1);
             matrizB[j] = Convert.ToInt32(Console.ReadLine());
         }
 
-        for (int x = 0; x < 5; x++)
+        for (int x = 0; x < matrizC.Length; x++)
         {
             matrizC[x] = matrizA[x] * matrizB[x];
         }
 
         Console.WriteLine();
 
-        for (int y = 0; y < 5; y++)
+        for (int y = 0; y < matrizC.Length; y++)
         {
             Console.WriteLine("Matriz A: {0} x {1} Matriz B = Matriz C: {2}", matrizA[y], matrizB[y], matrizC[y]);
         }
