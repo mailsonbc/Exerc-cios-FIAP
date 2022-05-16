cfc7eaf5e33c4c55ee7575fe272c75199b4cdddd
mailsonbc <mailsonbc@gmail.com>
2022-05-16 20:16:28 -0300

    Exercicios vetores

---
+++ calc-imc/Academia/ExercicioVetor02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] matrizA = new int[5];
+        int[] matrizB = new int[5];
+        int[] matrizC = new int[5];
+
+        Console.WriteLine("Digite 5 valores inteiros para a Matriz A:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Digite valor {0} ", i + 1);
+            matrizA[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Digite 5 valores inteiros para a Matriz B:");
+        for (int j = 0; j < 5; j++)
+        {
+            Console.WriteLine("Digite valor {0} ", j + 1);
+            matrizB[j] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for(int x = 0; x < 5; x++)
+        {
+            matrizC[x] = matrizA[x] * matrizB[x];
+        }
+
+        Console.WriteLine();
+
+        for (int y = 0; y < 5; y++)
+        {
+            Console.WriteLine("Matriz A: {0} x {1} Matriz B = Matriz C: {2}", matrizA[y], matrizB[y], matrizC[y]);
+        }
+    }
+}
---
+++ calc-imc/Academia/ExercicioVetores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] par = new int[15];
+        int[] impar = new int[15];
+        int[] geral = new int[15];
+        int x, ultimoIndicePar = 0, ultimoIndiceImpar = 0;
+
+        Console.WriteLine("Digite 15 valores inteiros: ");
+        for (int i = 1; i < geral.Length; i++)
+        {
+            Console.WriteLine("Digite o valor {0}: ", i);
+            x = Convert.ToInt32(Console.ReadLine());
+            if((x % 2) == 0)
+            {
+                par[ultimoIndicePar] = x;
+                geral[i] = x;
+                ultimoIndicePar++;
+            }
+            else
+            {
+                impar[ultimoIndiceImpar] = x;
+                geral[i] = x;
+                ultimoIndiceImpar++;
+            }
+        }
+
+        for (int j = 0; j < geral.Length; j++)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Números pares {0}, Números ímpares {1}, Todos os números {2}", par[j], impar[j], geral[j]);            
+            Console.WriteLine();
+        }
+    }
+}
