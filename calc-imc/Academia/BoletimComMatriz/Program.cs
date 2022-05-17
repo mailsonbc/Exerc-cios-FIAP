33334430ece89bbaf380dae03f63305a192b89c7
mailsonbc <mailsonbc@gmail.com>
2022-05-16 21:59:46 -0300

    Matrizes

---
+++ calc-imc/Academia/BoletimComMatriz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[]_0)
+    {
+        double[,] boletim = new double[5, 4];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Digite o ID do aluno:");
+            boletim[i, 0] = Convert.ToDouble(Console.ReadLine());
+            for (int j = 0; j < 4; j++)
+            {                
+                Console.WriteLine("Digite a nota do {0}° bimestre", j + 1);
+                boletim[i, j] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine("[ID do aluno {0}, Bimestre {1}] = Nota [{2}]", i + 1, j + 1, boletim[i, j]);
+            }
+        }
+    }
+}
---
+++ calc-imc/Academia/Matrizes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] matriz = new int[3,3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine("Preencha a matriz: ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine("[{0}, {1}] = [{2}]",i, j, matriz[i, j]);
+            }
+        }
+    }
+}
