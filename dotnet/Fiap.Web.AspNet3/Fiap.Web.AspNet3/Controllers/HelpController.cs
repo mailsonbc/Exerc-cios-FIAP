6a733fae779ca777822d5546feb43d21c0169e19
mailsonbc <mailsonbc@gmail.com>
2022-06-29 21:48:41 -0300

    teste de controllers

---
+++ dotnet/Fiap.Web.AspNet3/Fiap.Web.AspNet3/Controllers/HelpController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet3.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View("AboutUs");
+        }
+
+        public IActionResult Content()
+        {
+            //return Content("Retornando o conteúdo em formato string");
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
