6b664ccf37d0e8dbda62a6369e5cda88af607600
mailsonbc <mailsonbc@gmail.com>
2022-07-11 22:45:05 -0300

    Aula 04 .net

---
+++ dotnet/Fiap.Web.AspNet3/Fiap.Web.AspNet3/Migrations/20220712005642_RepresentanteNomeRequerido.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiap.Web.AspNet3.Migrations
+{
+    public partial class RepresentanteNomeRequerido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "Representante");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeRepresentante",
+                table: "Representante",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeRepresentante",
+                table: "Representante",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "Representante",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
